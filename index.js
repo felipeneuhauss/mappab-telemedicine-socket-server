8ac864121d4a80c5b35f25beb2095260417ba386
Felipe Araujo <FelipeAraujo.DECSKILL@edp.com>
2021-04-15 14:22:17 +0100

    feat: cors added

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const app = require('express')()
 const http = require('http').createServer(app)
+const cors = require('cors')
+app.use(cors())
 const io = require('socket.io')(http, {
   cors: {
-    origin: '*',      
+    origin: '*',
     credentials: false
   }
 })
